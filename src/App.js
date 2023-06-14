af16bce272d81c5eab45656be8c1ffb2edb3b2e0
sobhanessi <sobhan@esfandyari.com>
2023-06-14 22:15:21 +0200

    part one has been done

--- src/App.js
+++ src/App.js
@@ -18,22 +18,17 @@ const App = () => {
             <div className="container">
                 <h1 className="mt-3">Company's Budget Allocation</h1>
                 <div className="row mt-3">
-                    {/* Add Budget component here under */}
                     <div className="col-sm">
                         <Budget />
                     </div>
-                    {/* Add Remaining component here under */}
+
                     <div className="col-sm">
                         <Remaining />
                     </div>
-                    {/* Add ExpenseTotal component here under */}
+
                     <div className="col-sm">
                         <ExpenseTotal />
                     </div>
-                    {/* Add ExpenseList component here under */}
-
-                    {/* Add ExpenseItem component here under */}
-                    {/* Add AllocationForm component here under */}
                 </div>
                 <h3 className="mt-3">Allocation</h3>
                 <div className="row ">
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,42 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch, currency, remaining } =
+        useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const changeBudget = (budget) => {
+        if (budget > 20000)
+            alert(
+                "The value cannot exceed remaining funds" +
+                    { currency } +
+                    { remaining }
+            );
+        else if (budget < totalExpenses)
+            alert("The value cannot be lower than the spending");
+        else {
+            const expense = {
+                cost: parseInt(budget),
+            };
+            dispatch({
+                type: "SET_BUDGET",
+                payload: expense.cost,
+            });
+        }
+    };
+
     return (
         <div className="alert alert-secondary">
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}</span>
+            <input
+                type="number"
+                step={10}
+                value={budget}
+                onChange={(e) => changeBudget(e.target.value)}
+            />
         </div>
     );
 };
